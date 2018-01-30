5933e60aa3b0e4be617782f95acb7184948b8848
Vlad <kozlovskivlad@yandex.ru>
2018-01-30 19:45:07 +0300

    removed-toolbar

--- app/src/main/java/com/example/vlad81/kozlovskifirstapplication/LauncherViewActivity.java
+++ app/src/main/java/com/example/vlad81/kozlovskifirstapplication/LauncherViewActivity.java
@@ -30,15 +30,6 @@ public class LauncherViewActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_view);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
         createGridLayout();
     }
 
