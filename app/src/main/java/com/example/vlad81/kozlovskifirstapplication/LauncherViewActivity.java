04b100287d023bd9c288734d0ecce5adf71be535
Vlad <kozlovskivlad@yandex.ru>
2018-02-02 01:55:13 +0300

    fixing navigation

--- app/src/main/java/com/example/vlad81/kozlovskifirstapplication/LauncherViewActivity.java
+++ app/src/main/java/com/example/vlad81/kozlovskifirstapplication/LauncherViewActivity.java
@@ -121,28 +121,6 @@ public class LauncherViewActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navigation_view, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
@@ -154,9 +132,6 @@ public class LauncherViewActivity extends AppCompatActivity
             startActivity(toListActivity);
         } else if (id == R.id.settings) {
 
-        } else if (id == R.id.imageView) {
-            Intent toMyPage = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(toMyPage);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
