fe0ccec01c2324ec2adab302eeb3fb0853d3f6e6
Vlad <kozlovskivlad@yandex.ru>
2018-02-02 19:25:13 +0300

    i hate title color. added settings activity drawer

--- app/src/main/java/com/example/vlad81/kozlovskifirstapplication/SettingsActivity.java
+++ app/src/main/java/com/example/vlad81/kozlovskifirstapplication/SettingsActivity.java
@@ -1,21 +1,120 @@
 package com.example.vlad81.kozlovskifirstapplication;
 
-import android.app.Activity;
+import android.app.LauncherActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
-/**
- * Created by vlad81 on 2.2.18.
- */
 
-public class SettingsActivity extends Activity {
+import com.example.vlad81.kozlovskifirstapplication.launcher.LauncherAdapter;
+import com.example.vlad81.kozlovskifirstapplication.launcher.OffsetItemDecoration;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class SettingsActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    public SettingsActivity(){}
+
+    private NavigationView navigationView;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme();
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_view);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.post(new Runnable() {
+            @Override
+            public void run() {
+                navigationView.setCheckedItem(R.id.launcher_activity);
+            }
+        });
+
+        final View navigationHeaderView = navigationView.getHeaderView(0);
+        final View profileImage = navigationHeaderView.findViewById(R.id.imageView);
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(R.id.settings_fragment_container, new SettingsFragment())
                 .commit();
 
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.list_activity) {
+            Intent toListActivity = new Intent(getApplicationContext(), LinearLayoutActivity.class);
+            toListActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    | Intent.FLAG_ACTIVITY_NEW_TASK );
+            startActivity(toListActivity);
+        } else if (id == R.id.launcher_activity) {
+            Intent toLauncherActivity = new Intent();
+            toLauncherActivity.setClass(this, LauncherViewActivity.class);
+            toLauncherActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    | Intent.FLAG_ACTIVITY_NEW_TASK );startActivity(toLauncherActivity);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void setTheme() {
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
+        int thirdPageTheme = Integer.parseInt(sharedPreferences.getString("pref_Theme", "1"));
+
+        if (thirdPageTheme == 2) {
+            setTheme(R.style.DarkAppTheme_NoActionBar);
+        } else {
+            setTheme(R.style.AppTheme_NoActionBar);
+        }
     }
 }
